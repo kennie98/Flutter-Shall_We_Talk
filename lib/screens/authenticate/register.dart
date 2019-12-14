5e6646f8da50d3c7e35d224db014f8778261f486
Ken Tsoi <kennie98@gmail.com>
2019-12-14 17:39:12 -0500

    remove the work-around for the dropdown button bad behavior on register page

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -144,7 +144,6 @@ class _State extends State<SignUpBlock> {
   }
 
   Widget FormUI() {
-    final focus = FocusNode();
     return new Column(
       children: <Widget>[
         Row(
@@ -152,14 +151,12 @@ class _State extends State<SignUpBlock> {
             Expanded(
               child: new TextFormField(
                 style: global_style.ts,
-                focusNode: focus,
                 decoration: const InputDecoration(
                   labelText: 'Username',
                   helperText: '',
                 ),
                 keyboardType: TextInputType.text,
                 validator: _validateUsername,
-                onEditingComplete: focus.unfocus,
                 onSaved: (String val) {
                   _username = val;
                 },
@@ -176,7 +173,6 @@ class _State extends State<SignUpBlock> {
                 keyboardType: TextInputType.visiblePassword,
                 validator: _validatePassword,
                 obscureText: true,
-                onEditingComplete: focus.unfocus,
                 onSaved: (String val) {
                   _password = val;
                 },
@@ -195,7 +191,6 @@ class _State extends State<SignUpBlock> {
                 ),
                 keyboardType: TextInputType.text,
                 validator: _validateName,
-                onEditingComplete: focus.unfocus,
                 onSaved: (String val) {
                   _name = val;
                 },
@@ -248,7 +243,6 @@ class _State extends State<SignUpBlock> {
                 ),
                 keyboardType: TextInputType.number,
                 validator: _validateAge,
-                onEditingComplete: focus.unfocus,
                 onSaved: (String val) {
                   _age = val;
                 },
@@ -266,7 +260,6 @@ class _State extends State<SignUpBlock> {
                 ),
                 keyboardType: TextInputType.number,
                 validator: _validatePhoneNumber,
-                onEditingComplete: focus.unfocus,
                 onSaved: (String val) {
                   _phoneno = val;
                 },
